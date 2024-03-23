a6a6bc5b945e71cfc2d630dc2e6b3900a465ff05
khomotso gojela <motsoak49@gmail.com>
2024-03-23 07:21:24 +0200

    first commit

---
+++ task.dart
@@ -0,0 +1,22 @@
+void main() {
+  /**
+   * This is collection used to store multiple key value pairs.
+   */
+  Map<String, String> stuDetails = {
+    "karabo": "20",
+    "fefe": "30",
+    "danko": "40"
+  };
+
+  // A string data type is used to represent text
+  String msg = "You passed the test!";
+
+  // int data type is used to store whole numbers
+  int num = 20;
+
+  // double data type is used to store decimals
+  double num2 = 20.333;
+
+  // bool data type is used to store true/false values
+  bool isBoiling = true;
+}
